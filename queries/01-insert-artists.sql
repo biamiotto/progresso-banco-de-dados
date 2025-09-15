936c4a8eda7130860d61a36000c90ac3c3ff5de5
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2025-09-15 15:59:15 -0300

    Cria as queries de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,16 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Adele', 'Solo', 1, 'Pop', 2006, 'Reino Unido', 'Rolling in the Deep', TRUE),
+('21 Pilots', 'Banda', 2, 'Rock Alternativo', 2009, 'Estados Unidos', 'Stressed Out', TRUE),
+('Justin Bieber', 'Solo', 1, 'Pop', 2009, 'Canadá', 'Baby', TRUE),
+('BTS', 'Banda', 7, 'K-pop', 2013, 'Coreia do Sul', 'Dynamite', TRUE),
+('Coldplay', 'Banda', 4, 'Rock Alternativo', 1996, 'Reino Unido', 'Viva La Vida', TRUE),
+('Taylor Swift', 'Solo', 1, 'Pop/Country', 2006, 'Estados Unidos', 'Shake It Off', TRUE),
+('Rihanna', 'Solo', 1, 'Pop/R&B', 2005, 'Barbados', 'Umbrella', TRUE),
+('Imagine Dragons', 'Banda', 4, 'Pop Rock', 2008, 'Estados Unidos', 'Radioactive', TRUE),
+('Ed Sheeran', 'Solo', 1, 'Pop/Folk', 2011, 'Reino Unido', 'Shape of You', TRUE),
+('Maroon 5', 'Banda', 5, 'Pop Rock', 2001, 'Estados Unidos', 'Sugar', TRUE)
