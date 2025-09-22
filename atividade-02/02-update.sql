097ee751894ae408e6eac76638ccd69b654ca706
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2025-09-22 11:16:48 -0300

    Finaliza Atividade 2

---
+++ atividade-02/02-update.sql
@@ -0,0 +1,27 @@
+UPDATE produtos SET preco = preco * 0.8;
+
+SELECT * FROM produtos;
+
+UPDATE produtos SET estoque = estoque + 50;
+
+SELECT * FROM produtos;
+
+UPDATE produtos SET preco = preco * 1.15
+WHERE marca = 'Creamy';
+
+SELECT * FROM produtos;
+
+UPDATE produtos SET preco = preco - preco * 0.1;
+
+SELECT * FROM produtos;
+
+-- Desafio Extra:
+UPDATE produtos SET preco = preco * 0.3
+WHERE categoria = 'Perfumes';
+
+SELECT * FROM produtos;
+
+UPDATE produtos SET estoque = estoque + 25
+WHERE preco > 150;
+
+SELECT * FROM produtos;
